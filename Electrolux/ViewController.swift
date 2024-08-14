531b7bf09aa8ae9a0d8e3f7981b85d310e712606
ronesta <gvozzzd123@gmail.com>
2024-08-14 23:06:57 +0300

    first commit

---
+++ Electrolux/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Electrolux
+//
+//  Created by Ибрагим Габибли on 14.08.2024.
+//
+
+import UIKit
+import PureLayout
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
---
+++ Pods/Target Support Files/Pods-Electrolux/Pods-Electrolux-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Electrolux : NSObject
+@end
+@implementation PodsDummy_Pods_Electrolux
+@end
